b85ce2939d4eee2246303055cee27cb8a69b6d63
AnixDrone <tomislavignjatov@rocketmail.com>
2023-12-06 00:45:52 +0100

    remove target

--- day1/target/debug/deps/day1-d931dc90284a2563.d
+++
@@ -1,5 +0,0 @@
-/home/tomislav/Projects/aoc-23/day1/target/debug/deps/day1-d931dc90284a2563.rmeta: src/main.rs
-
-/home/tomislav/Projects/aoc-23/day1/target/debug/deps/day1-d931dc90284a2563.d: src/main.rs
-
-src/main.rs:
--- day1/target/debug/deps/day1-fb4d85703d95215c.d
+++
@@ -1,5 +0,0 @@
-/home/tomislav/Projects/aoc-23/day1/target/debug/deps/day1-fb4d85703d95215c.rmeta: src/main.rs
-
-/home/tomislav/Projects/aoc-23/day1/target/debug/deps/day1-fb4d85703d95215c.d: src/main.rs
-
-src/main.rs:
