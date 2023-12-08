ee4197728ef89fc435801e8332a473b21705b130
AnixDrone <tomislavignjatov@rocketmail.com>
2023-12-08 21:26:45 +0100

    solved day1 part 2

--- day1/src/bin/part1.rs
+++ day1/src/bin/part1.rs
@@ -7,15 +7,22 @@ fn main() {
         Err(_) => panic!("Failed to read file"),
     };
 
-    //let words = content.split("\n").collect::<Vec<&str>>();
-    //let numbers = words
-    //    .iter()
-    //    .map(|word| extract_numbers_from_word(word))
-    //    .map(|number| number.parse::<i32>().unwrap())
-    //    .collect::<Vec<i32>>();
-    //print!("Sum of numbers: {:?}", numbers.iter().sum::<i32>());
-    let result = extract_numbers_from_inccorect_words("one2twoxqzthree");
-    print!("Result: {:?}", result);
+    let words = content.split("\n").collect::<Vec<&str>>();
+    let numbers = words
+        .iter()
+        .map(|word| extract_numbers_from_inccorect_words(word))
+        .map(|number| number.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    print!("Sum of numbers: {:?}", numbers.iter().sum::<i32>());
+}
+
+fn fuzzy_match_word(word: &str, word1: &str) -> bool {
+    for (i, c) in word.chars().enumerate() {
+        if c != word1.chars().nth(i).unwrap() {
+            return false;
+        }
+    }
+    true
 }
 
 fn extract_numbers_from_word(word: &str) -> String {
@@ -28,6 +35,9 @@ fn extract_numbers_from_word(word: &str) -> String {
         Some(inner) => inner.to_string(),
         None => String::from("0"),
     };
+    if first_character == "0" {
+        return second_character + "0";
+    }
     return first_character + &second_character;
 }
 
@@ -48,14 +58,35 @@ fn extract_numbers_from_inccorect_words(word: &str) -> String {
     let mut result = String::new();
     for c in word.chars() {
         if c.is_numeric() {
+            buffer.clear();
+            result.push(c);
             continue;
         } else {
             buffer.push(c);
             if number_map.contains_key(&buffer[..]) {
                 result.push_str(number_map.get(&buffer[..]).unwrap());
-                buffer.clear();
+                buffer = buffer.chars().last().unwrap().to_string();
+            } else {
+                let mut flag = false;
+                for key in number_map.keys() {
+                    if fuzzy_match_word(&buffer, &key) {
+                        flag = true;
+                    }
+                }
+                if !flag {
+                    buffer.remove(0);
+                }
             }
         }
     }
-    return result;
+
+    let first_character = match result.chars().next() {
+        Some(inner) => inner.to_string(),
+        None => String::from("0"),
+    };
+    let second_character = match result.chars().last() {
+        Some(inner) => inner.to_string(),
+        None => String::from("0"),
+    };
+    return first_character + &second_character;
 }
