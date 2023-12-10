b5ad2bba0dad752dfa9303c9c0b150c0b64c3581
AnixDrone <tomislavignjatov@rocketmail.com>
2023-12-11 00:32:23 +0100

    solved d3p2

--- day3/src/bin/part1.rs
+++ day3/src/bin/part1.rs
@@ -1,13 +1,13 @@
 use std::{fs, usize};
 
 fn main() {
-    let schematic = fs::read_to_string("src/sample_input.txt").expect("Error reading file");
+    let schematic = fs::read_to_string("src/input.txt").expect("Error reading file");
     let lines = schematic
         .split("\n")
         .filter(|l| !l.is_empty())
         .collect::<Vec<&str>>();
     let mut numbers = vec![];
-    let mut coordinates: Vec<(usize, usize)> = Vec::new();
+    let mut coordinates: Vec<Vec<(usize, usize)>> = Vec::new();
     for (i, line) in lines.iter().enumerate() {
         let mut num_flag = false;
         let mut num_buffer: Vec<(usize, usize)> = vec![];
@@ -21,7 +21,7 @@ fn main() {
                         for (s, x) in &num_buffer {
                             num.push(get_value(*s, *x, &lines));
                         }
-                        coordinates.extend(num_buffer.iter());
+                        coordinates.push(num_buffer.clone());
                         numbers.push(num.parse::<usize>().unwrap());
                     }
                     num_buffer.clear();
@@ -34,19 +34,85 @@ fn main() {
                     for (s, x) in &num_buffer {
                         num.push(get_value(*s, *x, &lines));
                     }
-                    coordinates.extend(num_buffer.iter());
+                    coordinates.push(num_buffer.clone());
                     numbers.push(num.parse::<usize>().unwrap());
                 }
                 num_buffer.clear();
             }
         }
     }
-    println!("{:?}", numbers);
-    println!("{:?}", numbers.iter().sum::<usize>());
+    let mut star_coordinates: Vec<Vec<Vec<(usize, usize)>>> = Vec::new();
+    for (i, line) in lines.iter().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            if c == '*' {
+                let mut buffer_cord = vec![];
+                for cord_set in &coordinates {
+                    let mut triggered = false;
+                    for (ci, cj) in cord_set {
+                        if *ci + 1 == i && *cj == j {
+                            triggered = true;
+                            break;
+                        }
+                        if *ci == i && *cj + 1 == j {
+                            triggered = true;
+                            break;
+                        }
+                        if *ci > 0 && *ci - 1 == i && *cj == j {
+                            triggered = true;
+                            break;
+                        }
+                        if *cj > 0 && *ci == i && *cj - 1 == j {
+                            triggered = true;
+                            break;
+                        }
+                        if *ci + 1 == i && *cj + 1 == j {
+                            triggered = true;
+                            break;
+                        } else if *ci > 0 && *cj > 0 && *ci - 1 == i && *cj - 1 == j {
+                            triggered = true;
+                            break;
+                        } else if *ci > 0 && *ci - 1 == i && *cj + 1 == j {
+                            triggered = true;
+                            break;
+                        } else if *cj > 0 && *ci + 1 == i && *cj - 1 == j {
+                            triggered = true;
+                            break;
+                        }
+                    }
+                    if triggered {
+                        buffer_cord.push(cord_set.clone());
+                    }
+                }
+                star_coordinates.push(buffer_cord);
+            }
+        }
+    }
+
+    println!("Coordinates : {:?}", star_coordinates);
+    let star_numbers = star_coordinates
+        .iter()
+        .map(|c| get_value_from_coordinates(&c, &lines))
+        .filter(|c| c.len() > 1)
+        .collect::<Vec<Vec<usize>>>();
+    println!("Numbers : {:?}", star_numbers);
+    println!(
+        "Sum : {}",
+        star_numbers
+            .iter()
+            .map(|c| c.iter().copied().reduce(|a, b| a * b).unwrap())
+            .sum::<usize>()
+    );
+
+    let flatten_coordinates = star_coordinates
+        .iter()
+        .filter(|c| c.len() > 1)
+        .flatten()
+        .flatten()
+        .collect::<Vec<&(usize, usize)>>();
     let mut new_lines: Vec<String> = Vec::new();
     for (i, line) in lines.into_iter().enumerate() {
         let mut new_line = line.to_string();
-        for (ci, cj) in &coordinates {
+        for (ci, cj) in &flatten_coordinates {
             if i == *ci {
                 new_line.remove(*cj);
                 new_line.insert(*cj, 'X');
@@ -55,6 +121,23 @@ fn main() {
         new_lines.push(new_line);
     }
     println!("{}", new_lines.join("\n"));
+}
+
+fn get_value_from_coordinates(cord: &Vec<Vec<(usize, usize)>>, lines: &Vec<&str>) -> Vec<usize> {
+    let numbers = cord
+        .iter()
+        .map(|c| {
+            let mut num = String::new();
+            for (i, j) in c {
+                num.push(get_value(*i, *j, &lines));
+            }
+            num
+        })
+        .collect::<Vec<String>>()
+        .iter()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+    numbers
 }
 
 fn get_value(i: usize, j: usize, lines: &Vec<&str>) -> char {
